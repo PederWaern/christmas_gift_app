fa11612e826af3340650987e81e447cfd21cb869
Peder Waern <pederwaern@hotmail.com>
2017-12-15 10:14:20 +0100

    first commit

---
+++ src/main/java/com/hotmail/pederwaern/docker_backend/DockerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.hotmail.pederwaern.docker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBackendApplication.class, args);
+	}
+}
